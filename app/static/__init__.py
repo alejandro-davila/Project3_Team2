de23fcb82230f8e9d14936e458ef64ac4f0f0103
Jaxion08 <jaxion08@gmail.com>
2023-09-27 20:44:32 -0500

    modifying imports

--- app/static/__init__.py
+++ app/static/__init__.py
@@ -1,6 +1,6 @@
 # __init__.py
 from flask import Flask
-from .models import db
+from app.static.models import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yourdatabase.db'  # adjust the URI accordingly
