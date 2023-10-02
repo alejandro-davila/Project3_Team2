badfd82ae11ece25cc152620fbc23dbb4ba4f78b
alejandro-davila <alejandrodavila@me.com>
2023-10-02 18:51:23 -0500

    updating files

--- JS/logic.js
+++ JS/logic.js
@@ -1,4 +1,7 @@
 // Create a map object
+
+
+//---------------------------------------------------------- MAPS (STREET, TOPO, DARK) ----------------------------------------------------------
 function CreateMap(map){
 
   let street = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
@@ -8,121 +11,148 @@ function CreateMap(map){
     {attribution:
       'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'});
     
-  //let streetmap = L.map("map", {
-   // center: [31.00, -100.00],
-    //zoom: 7,
-    //layers: [street, map]});
  
+  
   let PUBLIC_API_KEY ="pk.eyJ1Ijoic2NvbHNvbjgyIiwiYSI6ImNrdTYzbjhrdjU3ODMyb28yZmlrMHpybjYifQ.jzpQ-HWh3lT55X-v0IQoHA";
-
-  let overlayMaps = {
-    // Add Tectonic Plates
-    "Diesel Stations": map,
-    "Electric Stations": map};
-
   let dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
     {attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
       accessToken: PUBLIC_API_KEY});
 
-  let myMap = L.map("map", {
-    center: [30.25, -97.75],
-    layers:[street,map],
-    zoom: 10
-  });
   let baseMaps = {
     "Street Map": street,
     "Topographic Map": topo,
     Dark: dark};
+ 
+  let myMap = L.map("map", {
+    center: [30.417, -100.810],
+    layers:[street,map],
+    zoom: 7});
 
-    // Adding the tile layer
-    L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
-      }).addTo(myMap);
-    var legend = L.control({ position: "bottomright" });
-    legend.onAdd = function () {
-    var div = L.DomUtil.create("div", "info legend");
-    var limits = [0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0];
-    var colors = ["#FFFF99","#FFFF00","#FFB266","#FF9933",
-        "#FF8000","#CC6600","#CC0000","#990000","#660000","#330000"];
 
-    // Add the minimum and maximum.
-    var legendInfo = `
-        <h3>Fuel Markers</h3>
 
-        <div class="labels">
-        <div class="min">
-        ${limits[0]}
-        </div>
-        
-        <div class="max">
-        ${limits[limits.length - 1]}
-        </div>
-        </div>
-        <ul>`;
 
-    limits.forEach(function (limit, index) {
-        legendInfo += '<li style="background-color: ' + colors[index] + '"></li>'});
 
-    legendInfo += "</ul>";
-    div.innerHTML = legendInfo;
-    return div};
-    // Adding the legend to the map
-    legend.addTo(myMap); 
-      
+
+//---------------------------------------------------------- Markers ----------------------------------------------------------
+    
+    let marker1 = {
+      "Tesla (Fast)": map,
+      "CCS/SAE": map,
+      "CHAdeMO": map,
+      "J-1772": map,
+      "Tesla": map,
+      "Tesla (Roadster)": map,
+      "NEMA 14-50": map,
+      "Wall": map,
+      "NEMA TT-30": map,
+    };
+    
+
+
+    
+  L.control.layers(baseMaps, null, {
+    collapsed: false
+  }).addTo(myMap);  
+
+  
+  // Create a control for overlay maps
+  let overlayControl = L.control.layers(null, marker1, {
+    collapsed: false});
+  overlayControl.setPosition('topright');
+  myMap.addControl(overlayControl);
+
+  let overlayControlContainer = document.createElement('div');
+
+  overlayControlContainer.appendChild(overlayControl.getContainer());
+  document.getElementById('overlayControlContainer').appendChild(overlayControlContainer);
+
+
+
+// Function to show a warning message when any marker is clicked
+function showWarningMessage(e) {
+  let marker = e.target;
+  
+  // Create a popup with the warning message content
+  let popupContent = `
+    <div class="warning-popup">
+      <img src="warning-icon.png" alt="Warning Icon" width="50" height="50">
+      <p>Are you sure?</p>
+    </div>
+  `;
+  
+  // Bind the popup to the marker
+  marker.bindPopup(popupContent).openPopup();
 }
 
-let url = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=RjZpx6DxeHBP4d17uK9Uifu6qhas3674psy6dJ7Q&limit=10&fuel_type=ELEC&state=TX"
-
-function markers(){
-    d3.json(url).then(function(data){
-        console.log(data);
-        let gasmarkers=[]
-        let stations = data.fuel_stations;
-
-        //let elevations=[];
-       // for (let i = 0; i < stations.length; i++) {
-        //    elevations.push(stations[i].geometry.coordinates[2])
-       // }
-        //let minimum=d3.min(elevations)
-       // let maximum=d3.max(elevations)
-       // console.log(minimum)
-       // console.log(maximum)
-       // let ColorScale=d3.scaleSequential(d3.interpolateRdYlGn).domain([90,minimum]);
-
-        for (let i = 0; i < stations.length; i++) {
-          //let location = stations[i].geometry;
-          let properties=stations[i].station_name
-          let address = stations[i].street_address
-          let address2 = stations[i].city
-          let address3 = stations[i].state  
-          let fueltype = stations[i].fuel_type_code
-          let long=stations[i].longitude;
-          let lat=stations[i].latitude;
-          //let elev=location.coordinates[2];
-          //let markeradius=properties.mag*3;
-         // let scalecolor= ColorScale(elev);
-
-         
-         if (fueltype === "ELEC") {
-          fueltype = "Electric";
-      } else if (fueltype === "Diesel") {
-          fueltype = "Diesel";
-      }
-
-         
-         let gasmarker = L.marker([lat, long])
-         .bindPopup("<h1>" + properties+ "<h1><h2>"+address+" "+address2+" "+address3+"</h2><h3>Fuel Type: " + fueltype + "</h3>");
+// Iterate through the marker1 object and add a click event listener to each marker
+for (let markerName in marker1) {
+  if (marker1.hasOwnProperty(markerName)) {
+    let marker = marker1[markerName];
+    marker.addEventListener('click', showWarningMessage);
+  }
+}}
 
 
 
-          gasmarkers.push(gasmarker);
-
-        }
-        console.log(gasmarkers)
-        CreateMap(L.layerGroup(gasmarkers))})}
 
 
 
-d3.json("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=RjZpx6DxeHBP4d17uK9Uifu6qhas3674psy6dJ7Q&limit=10&fuel_type=ELEC&state=TX").then(markers);
+
+//---------------------------------------------------------- JSON reader ----------------------------------------------------------
+let fuelurl = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=RjZpx6DxeHBP4d17uK9Uifu6qhas3674psy6dJ7Q&limit=10&fuel_type=ELEC&state=TX"
+
+function markers() {
+  d3.json(fuelurl).then(function (data) {
+    console.log(data);
+    let gasmarkers = [];
+    let stations = data.fuel_stations;
+    
+    // Define icon URLs for different fuel types
+    var iconMapping = {
+      ELEC: 'icons/electric-icon.png',
+      CNG: 'icons/cng-icon.png',
+      LNG: 'icons/lng-icon.png',
+      HY: 'icons/hydrogen-icon.png',
+      BD: 'icons/biodiesel-icon.png',
+      E85: 'icons/e85-icon.png',
+      LPG: 'icons/lpg-icon.png',
+    };
+
+    for (let i = 0; i < stations.length; i++) {
+      let properties = stations[i].station_name;
+      let fueltype = stations[i].fuel_type_code;
+      let long = stations[i].longitude;
+      let lat = stations[i].latitude;
+
+      // Select the appropriate icon URL based on the fuel type
+      var iconUrl = iconMapping[fueltype] || 'icons/default-icon.png';
+
+      let gasmarker = L.marker([lat, long], { 
+        icon: L.icon({
+          iconUrl: iconUrl,
+          iconSize: [38, 95],
+          shadowSize: [50, 64],
+          iconAnchor: [19, 64],
+          shadowAnchor: [4, 62],
+          popupAnchor: [-3, -76],
+        })
+      }).bindPopup("<h3>" + properties + "</h3><h3>Fuel Type: " + fueltype + '</h3>');
+
+      gasmarkers.push(gasmarker);
+    }
+
+    CreateMap(L.layerGroup(gasmarkers));
+  });
+}
+
+
+
+//---------------------------------------------------------- PRODUCES MAP ON SITE (DO NOT DELETE!!)----------------------------------------------------------
+d3.json('https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=RjZpx6DxeHBP4d17uK9Uifu6qhas3674psy6dJ7Q&limit=10&fuel_type=ELEC&state=TX').then(markers)
+//---------------------------------------------------------- PRODUCES MAP ON SITE (DO NOT DELETE!!)----------------------------------------------------------
+
+
+
+
