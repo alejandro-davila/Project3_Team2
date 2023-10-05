77b12def7408a4c8623019821319ab87885e1a51
alejandro-davila <alejandrodavila@me.com>
2023-10-05 18:50:40 -0500

    updating app.py file

--- Python/app.py
+++ Python/app.py
@@ -4,7 +4,13 @@ import pandas as pd
 from flask import Flask, jsonify
 from sqlalchemy import create_engine, Column, Integer, Text, Float, Boolean, Date
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
-from flask_cors import CORS
+
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+from flask import send_file
+from io import BytesIO
 
 
 # Fetching data from the API
@@ -71,7 +77,6 @@ df.to_sql('fuel_stations', con=engine, if_exists='replace', index=False)
 
 # Flask Setup
 app = Flask(__name__)
-CORS(app)
 db_session = scoped_session(sessionmaker(bind=engine))
 
 # Reflect db tables using automap_base
@@ -185,9 +190,102 @@ def get_station(station_id):
 @app.teardown_appcontext
 def cleanup(resp_or_exc):
     db_session.remove()
+    
+@app.route('/stations_per_year', methods=['GET'])
+def stations_per_year():
+        # Query the data
+    station_dates = db_session.query(FuelStation.open_date).all()
+    
+    # Convert to DataFrame
+    df_dates = pd.DataFrame(station_dates, columns=["open_date"])
+    
+    # Drop rows where open_date is NaN
+    df_dates = df_dates.dropna(subset=['open_date'])
+
+    # Extract the year and convert to integer
+    df_dates['year'] = pd.DatetimeIndex(df_dates['open_date']).year.astype(int)
+    
+    yearly_counts = df_dates.groupby('year').size()
+
+    # Plot
+    plt.figure(figsize=(10,6))
+    ax = yearly_counts.plot(kind="bar")
+    
+    # Explicitly set x-ticks and x-ticklabels
+    ax.set_xticks(range(len(yearly_counts)))
+    ax.set_xticklabels(yearly_counts.index, rotation=45)
+
+    plt.title("Number of Stations Opened Per Year")
+    plt.xlabel("Year")
+    plt.ylabel("Number of Stations")
+    
+    # Save to BytesIO object in PNG format
+    img = BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+
+    # Return as image
+    return send_file(img, mimetype='image/png')
+
+@app.route('/chargers_in_cities', methods=['GET'])
+def chargers_in_cities():
+    # Filter the DataFrame to include only Austin, Houston, and San Antonio
+    selected_cities = ['Austin', 'Houston', 'San Antonio']
+    filtered_df = df[df['city'].isin(selected_cities)]
+
+    # Group by city and count the number of chargers in each city
+    city_charger_counts = filtered_df['city'].value_counts()
+    
+    # Sort the cities by the number of chargers in descending order
+    city_charger_counts = city_charger_counts.sort_values(ascending=False)
+    
+    # Create a bar chart
+    plt.figure(figsize=(10, 6))
+    city_charger_counts.plot(kind='bar')
+    plt.title('Number of Chargers in Austin, Houston, and San Antonio')
+    plt.xlabel('City')
+    plt.ylabel('Number of Chargers')
+    
+    # Save to BytesIO object in PNG format
+    img = BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    
+    # Return as image
+    return send_file(img, mimetype='image/png')
+
+@app.route('/facility_type_distribution', methods=['GET'])
+def facility_type_distribution():
+    # Query the facility types from the database
+    facility_types = db_session.query(FuelStation.facility_type).all()
+    
+    # Convert to DataFrame
+    df_facility = pd.DataFrame(facility_types, columns=["facility_type"])
+    
+    # Count the occurrences of each facility type
+    facility_type_counts = df_facility['facility_type'].value_counts()
+    
+    # Create a pie chart
+    plt.figure(figsize=(8, 8))
+    facility_type_counts.plot(kind='pie', autopct='%1.1f%%')
+    plt.title('Facility Type Distribution')
+    plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.tight_layout()
+    
+    # Save to BytesIO object in PNG format
+    img = BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    
+    # Return as image
+    return send_file(img, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True,port=6660)
+
+
+# if __name__ == '__main__':
+    # app.run(debug=True,port=6660)
 
 
 
